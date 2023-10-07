ba39284e998658cdc3dbd4a11f102a5132a59245
seidasoeun <seida.soeun@gmail.com>
2023-10-07 18:41:11 +0700

    initial project

---
+++ app/src/main/java/kh/edu/rupp/ite/mad_group4_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package kh.edu.rupp.ite.mad_group4_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
