6c6d5cd170283db04aafc219b80929232327b4bd
Sereyvathanak <sereyvathanak15@gmail.com>
2023-11-30 02:26:18 +0700

    hilf

---
+++ app/src/main/java/kh/edu/rupp/ite/tinhewan2/MainActivity.kt
@@ -0,0 +1,11 @@
+package kh.edu.rupp.ite.tinhewan2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_profile)
+    }
+}
