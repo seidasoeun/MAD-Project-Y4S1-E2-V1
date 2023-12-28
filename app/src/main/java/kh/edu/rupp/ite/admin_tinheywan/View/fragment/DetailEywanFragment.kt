991add85aa24cdcc8c05240f1e7aa203d39face6
SOEUN Seida <98793161+seidasoeun@users.noreply.github.com>
2023-12-28 12:51:19 +0700

    der hx ham pas

--- app/src/main/java/kh/edu/rupp/ite/admin_tinheywan/View/fragment/DetailEywanFragment.kt
+++ app/src/main/java/kh/edu/rupp/ite/admin_tinheywan/View/fragment/DetailEywanFragment.kt
@@ -1,5 +1,6 @@
 package kh.edu.rupp.ite.admin_tinheywan.View.fragment
 
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -14,11 +15,15 @@ import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.FitCenter
+import com.google.gson.JsonObject
+import kh.edu.rupp.ite.admin_tinheywan.Model.ApiService
+import kh.edu.rupp.ite.admin_tinheywan.Model.RetrofitAuth
 import kh.edu.rupp.ite.admin_tinheywan.View.UserActivity
 import kh.edu.rupp.ite.admin_tinheywan.ViewModel.MVVM
 import kh.edu.rupp.ite.admin_tinheywan.databinding.FragmentUserDetaileywanBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class DetailEywanFragment : Fragment() {
 
@@ -91,6 +96,46 @@ class DetailEywanFragment : Fragment() {
             }
         }
 
+        // Add to cart
+        binding!!.detailBtn.setOnClickListener {
+            val sharedPreferences2 = requireActivity().getSharedPreferences("localstorage", Context.MODE_PRIVATE)
+            var token2 = sharedPreferences2.getString("TOKEN","").toString()
+            var apiService2: ApiService = RetrofitAuth.getInstance(token2).create(ApiService::class.java)
+
+            val body = JsonObject().apply {
+                addProperty("title",intent?.getString("detail_title").toString())
+                addProperty("description",intent?.getString("detail_description").toString())
+                addProperty("type",intent?.getString("detail_type").toString())
+                addProperty("price",intent?.getString("detail_price").toString())
+                addProperty("status",intent?.getString("detail_status").toString())
+                addProperty("eywan_id", intent?.getString("detail_id")!!.toInt())
+            }
+
+            runBlocking {
+                val task = apiService2.getcart(body)
+                if (task.isSuccessful){
+                    Log.d("oooo","Successful api")
+                    showAlertDialog("ALERT","SUCCESSFUL ADD TO CART",requireContext())
+                }else{
+                    Log.d("oooo","Error api")
+                }
+            }
+
+        }
+
+    }
+
+    private fun showAlertDialog(title:String,message:String,context:Context){
+        val alertDialogBuilder = AlertDialog.Builder(context)
+        alertDialogBuilder.setTitle(title)
+        alertDialogBuilder.setMessage(message)
+        alertDialogBuilder.setPositiveButton("OK"){
+                dialog, which ->
+            // do something when click ok
+        }
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 
 
--- app/src/main/java/kh/edu/rupp/ite/tinheywan/HomeFragment.kt
+++
@@ -1,4 +0,0 @@
-package kh.edu.rupp.ite.tinheywan
-
-class HomeFragment {
-}
